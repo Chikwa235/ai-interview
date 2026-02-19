dbfb09e8224b826894100dad75e5475866118f56
Chikwa235 <chisapachikwa25@gmail.com>
2026-02-19 23:02:41 +0200

    implement auth and storage

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import Agent from "@/app/components/Agent";
+
 
 const page = () => {
   return (
-    <div>
-      page
-    </div>
+    <>
+        <h3>Interview Page</h3>
+        <Agent userName="You" userId="user1" type="generate"/>
+    </>
   )
 }
 
